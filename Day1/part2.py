97ae9a74a3337fd9759c119853773c6405340b54
echomicke <michael.kamara@chasacademy.se>
2023-01-15 19:21:52 +0100

    Completed the AOC challange for Day1 part2

--- Day1/part2.py
+++ Day1/part2.py
@@ -1,7 +1,7 @@
 def Calculate_fuel(mass):
     return (mass // 3) - 2
 
-
+"""Returns the sum of fuel required by all modules"""
 def Total_required_fuel(module_mass):
     fuel_sum = 0
     for i in module_mass:
@@ -15,3 +15,11 @@ def Total_required_fuel(module_mass):
             temp_fuel_module = temp_fuel
     
     return fuel_sum
+
+input_file = open("input")
+fuel_input = input_file.read()
+input_file.close()
+
+fuel_input = fuel_input.splitlines()
+fuel_input = list(map(int, fuel_input))
+print(Total_required_fuel(fuel_input))
