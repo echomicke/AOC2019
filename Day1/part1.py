3413472e296bec6b84dfb66c98b8aa4e915f3de2
echomicke <michael.kamara@chasacademy.se>
2023-01-15 15:55:21 +0100

    Refactored the fuel required method

--- Day1/part1.py
+++ Day1/part1.py
@@ -1,7 +1,5 @@
 """Calculates and returns the amount of fuel required based on the mass"""
 def Fuel_required(mass):
     fuel = mass // 3
-    fuel = fuel - 2
-
-    return fuel
+    return fuel - 2
 
