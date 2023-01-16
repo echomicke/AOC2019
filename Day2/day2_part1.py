f738a07ef257d69847f12d860e94975f08bf5991
echomicke <michael.kamara@chasacademy.se>
2023-01-16 15:22:33 +0100

    Added the process_intcode method for day2 part1 to pass the second test

--- Day2/day2_part1.py
+++ Day2/day2_part1.py
@@ -3,3 +3,28 @@ def pre_run(intcode):
     intcode[2] = 2
 
     return intcode
+
+def process_intcode(intcode):
+    int_pointer = 0
+    #processed_intcode = intcode
+    for i in range(len(intcode)):
+        i = int_pointer
+        x = intcode[i + 1]
+        y = intcode[i + 2]
+        z = intcode[i + 3]
+
+        if intcode[i] == 1:
+            intcode[z] = x + y
+
+        elif intcode[i] == 2:
+            intcode[z] = x * y
+
+        elif intcode[i] == 99:
+            break
+        int_pointer += 4
+
+        print(len(intcode)+3)
+        if int_pointer + 3 >= len(intcode):
+            break
+
+    return intcode
