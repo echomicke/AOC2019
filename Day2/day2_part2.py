b27a3ef5365c116b0c63f804f944dbdaa7a98d90
echomicke <michael.kamara@chasacademy.se>
2023-01-16 20:14:54 +0100

    Made some adjustments and completed the challange

--- Day2/day2_part2.py
+++ Day2/day2_part2.py
@@ -1,24 +1,26 @@
 def process_intcode(intcode):
-    for i in range(0, len(intcode), 4):
-        if i + 3 >= len(intcode):
+    intcode_arr = intcode[:]
+
+    for i in range(0, len(intcode_arr), 4):
+        if i + 3 >= len(intcode_arr):
             break
 
-        x = intcode[intcode[i + 1]]
-        y = intcode[intcode[i + 2]]
-        z = intcode[i + 3]
+        x = intcode_arr[intcode_arr[i + 1]]
+        y = intcode_arr[intcode_arr[i + 2]]
+        z = intcode_arr[i + 3]
 
-        if intcode[i] == 1:
-            intcode[z] = x + y
+        if intcode_arr[i] == 1:
+            intcode_arr[z] = x + y
 
-        elif intcode[i] == 2:
-            intcode[z] = x * y
+        elif intcode_arr[i] == 2:
+            intcode_arr[z] = x * y
 
-        elif intcode[i] == 99:
+        elif intcode_arr[i] == 99:
             break
 
-    return intcode
+    return intcode_arr
 
-def find_Noun_verb(intcode, targetOutput):
+def find_Noun_verb(intcode, targetOutputt):
     result = 0
     for noun in range(100):
         for verb in range(100):
@@ -26,11 +28,23 @@ def find_Noun_verb(intcode, targetOutput):
             intcode[2] = verb
             output = process_intcode(intcode)
 
-            if output[0] == targetOutput:
+            if output[0] == targetOutputt:
                 result = 100 * noun + verb
                 break
 
-        if output[0] == targetOutput:
+        if output[0] == targetOutputt:
             break
 
     return result
+
+
+
+input_file = open("day2_input")
+intcode_input = input_file.read()
+input_file.close()
+
+intcode_input = intcode_input.split(',')
+intcode_input = list(map(int, intcode_input))
+
+results = find_Noun_verb(intcode_input, 19690720)
+print(results)
