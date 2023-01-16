e97a53038bd5af5a14cbcfe8a859d9f7a2a16852
echomicke <michael.kamara@chasacademy.se>
2023-01-16 19:09:59 +0100

    Added the second test method for day2 part2

--- Day2/test_day2_part2.py
+++ Day2/test_day2_part2.py
@@ -11,3 +11,8 @@ class TestStringMethods(unittest.TestCase):
         result = pt2.process_intcode(self.example_input)
 
         self.assertEqual(expected_value, result)
+    
+    def test_find_noun_verb(self):
+        target_output = 30
+        result = pt2.find_Noun_verb(self.example_input, target_output)
+        self.assertEqual(result, 0)
