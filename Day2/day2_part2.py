2a17eface40bc51c2dd4d562f0cfad4336f87591
echomicke <michael.kamara@chasacademy.se>
2023-01-16 18:35:11 +0100

    Added the process_intcode method for day2 part2

---
+++ Day2/day2_part2.py
@@ -0,0 +1,19 @@
+def process_intcode(intcode):
+    for i in range(0, len(intcode), 4):
+        if i + 3 >= len(intcode):
+            break
+
+        x = intcode[intcode[i + 1]]
+        y = intcode[intcode[i + 2]]
+        z = intcode[i + 3]
+
+        if intcode[i] == 1:
+            intcode[z] = x + y
+
+        elif intcode[i] == 2:
+            intcode[z] = x * y
+
+        elif intcode[i] == 99:
+            break
+
+    return intcode
